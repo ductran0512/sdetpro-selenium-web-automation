e5cd05fad755a4d1d8c5a7e688c6845cce07ecca
ductran0512 <minhducvt0512@gmail.com>
2023-11-18 19:32:42 +0700

    Day 14, Js Executor

---
+++ sdetpro-selenium-automation-web/src/main/java/api_learning/JsExecutor.java
@@ -0,0 +1,36 @@
+package api_learning;
+
+import driver.DriverFactory;
+import org.openqa.selenium.*;
+
+public class JsExecutor {
+    private static final String TARGET_URL = "https://the-internet.herokuapp.com/floating_menu";
+    private static final String TO_BOTTOM_JS_SNIPPET = "window.scrollTo(0, document.body.scrollHeight);";
+    private static final String TO_TOP_JS_SNIPPET = "window.scrollTo(document.body.scrollHeight, 0);";
+
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.getWebDriver();
+        driver.get(TARGET_URL);
+
+        try {
+            // Scroll to bottom
+            JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+            javascriptExecutor.executeScript(TO_BOTTOM_JS_SNIPPET);
+
+            // Scroll to top
+            javascriptExecutor.executeScript(TO_TOP_JS_SNIPPET);
+
+            // Make a 4px solid red border around the menu element
+            javascriptExecutor.executeScript("arguments[0].setAttribute('style', 'border: 4px solid red;');", driver.findElement(By.tagName("h3")));
+            javascriptExecutor.executeScript("arguments[0].removeAttribute('target')", driver.findElement(By.cssSelector("[href=\"http://elementalselenium.com/\"]")));
+            driver.findElement(By.cssSelector("[href=\"http://elementalselenium.com/\"]")).click();
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+
+    }
+
+}
